c53d5c1d416adb5a9b54b06bd0dbc16ef8e66d3b
vrindas <vrindas@amdocs.com>
2024-08-01 14:17:15 +0530

    initialize a map and add openStreetMap tiles to the map

--- public/js/script.js
+++ public/js/script.js
@@ -20,4 +20,9 @@ if(navigator.geolocation){
 }
 );
 }
- 
+
+const map = L.map("map").setView([0,0],10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+	attribution:"OpenStreetMap"
+}).addTo(map)
