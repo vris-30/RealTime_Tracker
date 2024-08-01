fb50f2eddba051f865d03c88cd07181df8254ab4
vrindas <vrindas@amdocs.com>
2024-08-01 12:34:49 +0530

    setting up the blueprint for socket.io

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const http = require("http");
+
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+app.get("/", function (req, res){
+	res.send("starting");
+});
+
+server.listen(3000);
