d5a617da48e203db47cbbbfeb2ade6502a951abe
vrindas <vrindas@amdocs.com>
2024-08-01 14:06:10 +0530

    checking geo-location and set options

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,23 @@
 const socket = io();
 
-console.log("starting")
+// check if the browser supports geo location
+// and use watch position to tract the users location continously.
+
+if(navigator.geolocation){
+	navigator.geolocation.watchPosition((position)=>{
+		const {latitiude, longitude} = position.coords;
+		socket.emit("send-location", {latitude , longitude});
+}, (error)=>{
+	console.error(error);
+},
+
+// set options for high accuracy, 
+// a 5-second timeout, and no caching.
+{
+	enableHighAccuracy: true,
+	timeout:5000,
+	maximumAge: 0
+}
+);
+}
+ 
